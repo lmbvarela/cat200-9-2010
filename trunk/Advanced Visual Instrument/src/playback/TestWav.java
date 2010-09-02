64fedae7b8b9850e512e6a8f1ea3a8687ec490cc
yeongchuin17 <yeongchuin17@dbf7061f-3d73-556f-6809-4f9ba60ca3d6>
2010-09-02 07:08:22 +0000

    Base code for playback and mixing module

---
+++ trunk/Advanced Visual Instrument/src/playback/TestWav.java
@@ -0,0 +1,16 @@
+package playback;
+
+/**
+ * Based on
+ *TestWav.java
+ *@author Quasar063501
+ *@version 0.1
+ *   */
+public class TestWav {
+public static void main(String args[]) {
+Playback p = new Playback("src/sound.wav");
+
+p.start();
+
+}
+}
