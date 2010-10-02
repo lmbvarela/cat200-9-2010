e7037685b55ce32a0bc7fb2c91cbefe5542f6ea6
seahling90 <seahling90@dbf7061f-3d73-556f-6809-4f9ba60ca3d6>
2010-10-02 12:16:32 +0000

    Keyboard features added

--- trunk/Advanced Visual Instrument/src/Piano.java
+++ trunk/Advanced Visual Instrument/src/Piano.java
@@ -1,49 +1,113 @@
 // CAT 200 Group Project
 // Group 9 : Advanced Visual Instruments
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.sound.midi.Instrument;
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Synthesizer;
-import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
 
-
-import java.awt.*;
-import java.awt.event.*;
 
 public class Piano extends JInternalFrame{
 	
 	private MidiChannel channel;
 	private Synthesizer synthesizer;
+	private boolean opt;
 	JComboBox instruments;
+	JRadioButton mouseOpt;
+	JRadioButton keyboardOpt;
+	ButtonGroup optGroup;
+	JLabel instruction;
+	JPanel optPanel;
+	Box optBox = Box.createHorizontalBox();
+	layout keyboard;
+
 	
   public static void main(String[] args) {
-    new Piano();
+	  new Piano();
   }
 
   public Piano(){
     setTitle("Virtual Piano");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(new Dimension(1083,200));
+    setSize(new Dimension(1083,250));
     setClosable(true);
-    setResizable(true);
+    setResizable(false);
     setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    
     insInit();
     instruments = new JComboBox (getInstruments());
-    getContentPane ().add (instruments, BorderLayout.NORTH);
-    getContentPane().add(new layout());
+    
+    mouseOpt = new JRadioButton("Mouse Mode");
+    keyboardOpt = new JRadioButton("Keyboard Mode");
+    optGroup = new ButtonGroup();
+    optGroup.add(mouseOpt);
+    optGroup.add(keyboardOpt);
+    mouseOpt.setSelected(true); //Default option is mouse
+    opt = true;
+     
+    optBox.add(mouseOpt);
+    optBox.add(Box.createHorizontalStrut(10));
+    optBox.add(keyboardOpt);
+    
+    optPanel = new JPanel(); //Create a new JPanel to hold the instrument combo box and mode radio button
+    optPanel.setLayout(new BoxLayout(optPanel,BoxLayout.Y_AXIS));
+    optBox.setAlignmentX(LEFT_ALIGNMENT);
+    optPanel.add(instruments); 
+    optPanel.add(optBox);
+    getContentPane().add(optPanel, BorderLayout.NORTH);
+    
+    instruction = new JLabel("Press 'z' to switch to the left octave or 'x' to switch to the right octave for keyboard mode");
+    getContentPane().add(instruction, BorderLayout.PAGE_END);
+    
+    keyboard = new layout();
+    getContentPane().add(keyboard);
+    
     setVisible(true); 
     
     ActionListener al;
-    al = new ActionListener ()
-         {
+    al = new ActionListener (){
              public void actionPerformed (ActionEvent e)
              {
                 JComboBox cb = (JComboBox) e.getSource ();
                 chooseInstrument (cb.getSelectedIndex ());
+                if(opt == false) //Make sure focus to keyboard is not disturbed
+                keyboard.requestFocus();
              }
          };
     instruments.addActionListener (al);
+    
+    mouseOpt.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent e){
+    		opt = true;
+    		keyboard.repaint();
+    	}
+    });
+    
+    keyboardOpt.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent e){
+    		opt = false;
+    		keyboard.requestFocus();
+    		keyboard.repaint();
+    	}
+    });
   }
   
   public boolean chooseInstrument (int instrumentID)
@@ -60,14 +124,11 @@ public class Piano extends JInternalFrame{
      if (synthesizer == null)
          return null;
 
-
      Instrument [] instruments = synthesizer.getLoadedInstruments ();
      String [] ins = new String [instruments.length];
 
-
      for (int i = 0; i < instruments.length; i++)
           ins [i] = instruments [i].toString ();
-
 
      return ins;
   }
@@ -95,7 +156,6 @@ public class Piano extends JInternalFrame{
      // get instruments from default sound bank
      synthesizer.loadAllInstruments (synthesizer.getDefaultSoundbank ());
 
-
      // Obtain the set of MIDI channels
      MidiChannel [] midiChannel = synthesizer.getChannels ();
     
@@ -112,21 +172,31 @@ public class Piano extends JInternalFrame{
   }
 
 
-  public class layout extends JPanel implements MouseListener{
+  public class layout extends JPanel implements MouseListener, KeyListener{
 	  
+	  private int width;
+	  private int height;
+	  private int whiteKey;
+	  private int blackKey;
+	  private int[] whiteKeyStartX = new int[49];
+	  private int[] whiteKeyStopX = new int[49];
+	  private int[] blackKeyStartX = new int[35];
+	  private int[] blackKeyStopX = new int[35];
 	  int key;
+	  int octaveNum = 3;
+	  boolean keyFound = false;
 	  
 	  public void layout(){
 		  addMouseListener(this);
+		  addKeyListener(this);
 	  }
 	  
     public void paint(Graphics g){
-    	
     	key = 0;
     	
     	//draw piano
-    	int width = getSize().width; 
-        int height = getSize().height;
+    	width = getSize().width; 
+        height = getSize().height;
 
         g.setColor(Color.gray);
         g.fillRect(0, 0, width, height);
@@ -134,7 +204,7 @@ public class Piano extends JInternalFrame{
         g.setColor(Color.black);
         g.fillRect(0, (int)(height*0.3), width, (int)(height*0.7));
         
-        int whiteKey = (width / 49) + 1;
+        whiteKey = (width / 49) + 1;
 
         g.setColor(Color.white);
         for (int i = 0; i < 49; i++)
@@ -144,7 +214,7 @@ public class Piano extends JInternalFrame{
         	whiteKeyStopX[i] = i*whiteKey + (whiteKey - 1);
         }
         
-        int blackKey = width / 147;
+        blackKey = width / 147;
         int invalidKey = 1;
         int blackKeyCount = 0;
         
@@ -161,24 +231,91 @@ public class Piano extends JInternalFrame{
         	else if (invalidKey ==7)
         		invalidKey = 0;
         	invalidKey++;
-        }	
+        }
+        
+        if(opt == false){
+        	g.setColor(Color.blue);
+        	g.drawRect(whiteKeyStartX[(octaveNum * 7)],(int)(height*0.3), 7*whiteKey,(int)(height*0.7));
+        	g.drawRect(whiteKeyStartX[(octaveNum * 7)]-1,(int)(height*0.3)-1, 7*whiteKey+2,(int)(height*0.7)+2);
+        	g.drawString("S", whiteKeyStartX[(octaveNum * 7)] + 7, (int)(height*0.9));
+        	g.drawString("D", whiteKeyStartX[(octaveNum * 7) + 1] + 7, (int)(height*0.9));
+        	g.drawString("F", whiteKeyStartX[(octaveNum * 7) + 2] + 7, (int)(height*0.9));
+        	g.drawString("J", whiteKeyStartX[(octaveNum * 7) + 3] + 7, (int)(height*0.9));
+        	g.drawString("K", whiteKeyStartX[(octaveNum * 7) + 4] + 7, (int)(height*0.9));
+        	g.drawString("L", whiteKeyStartX[(octaveNum * 7) + 5] + 7, (int)(height*0.9));
+        	g.drawString(";", whiteKeyStartX[(octaveNum * 7) + 6] + 7, (int)(height*0.9));
+        	g.drawString("E", blackKeyStartX[(octaveNum * 5)] + 3, (int)(height*0.6));
+        	g.drawString("R", blackKeyStartX[(octaveNum * 5) + 1] + 3, (int)(height*0.6));
+        	g.drawString("I", blackKeyStartX[(octaveNum * 5) + 2] + 3, (int)(height*0.6));
+        	g.drawString("O", blackKeyStartX[(octaveNum * 5) + 3] + 3, (int)(height*0.6));
+        	g.drawString("P", blackKeyStartX[(octaveNum * 5) + 4] + 3, (int)(height*0.6));
+        }
     }// end paint
  
-  public void mousePressed (MouseEvent evt){
-	  
-	  int width = getSize().width;
-	  int height = getSize().height;
-
+    public void pressBlack(int keyNum){
+    	Graphics drawKey = getGraphics();
+    	drawKey.setColor(Color.getHSBColor(0.61F, 0.67F, 1.0F));
+    	drawKey.fillRect(blackKeyStartX[keyNum] + 1, (int)(height*0.3), 2*blackKey - 2, (int)(height*0.38));
+		
+		  //black key note assigning
+		  if (keyNum % 5 == 0)
+			  key = 1 + 12 * (keyNum / 5);
+		  else if (keyNum % 5 == 1)
+			  key = 3 + 12 * (keyNum / 5);
+		  else if (keyNum % 5 == 2)
+			  key = 6 + 12 * (keyNum / 5);
+		  else if (keyNum % 5 == 3)
+			  key = 8 + 12 * (keyNum / 5);
+		  else if (keyNum % 5 == 4)
+			  key = 10 + 12 * (keyNum / 5);
+		  
+		  drawKey.dispose();
+		  channel.noteOn (key + 24, 100);     //play notes
+    }
+    
+    public void pressWhite(int keyNum){
+    	Graphics drawKey = getGraphics();
+    	drawKey.setColor(Color.getHSBColor(0.61F, 0.67F, 1.0F));
+    	drawKey.fillRect(whiteKeyStartX[keyNum], (int)(height*0.3), whiteKey - 1, (int)(height*0.7)); 
+		
+		drawKey.setColor(Color.black);
+			
+		if((keyNum + 1) % 7 == 1 || (keyNum + 1) % 7 == 4)           //Draw black key on the right
+			drawKey.fillRect(whiteKeyStartX[keyNum] + 2*blackKey, (int)(height*0.3), 2*blackKey, (int)(height*0.38));
+		else if ((keyNum + 1) % 7 == 0 || (keyNum + 1) % 7 == 3)     //Draw black key on the left
+			drawKey.fillRect(whiteKeyStartX[keyNum], (int)(height*0.3), blackKey - 1, (int)(height*0.38));
+		else 	//draw black key on the centre
+		{
+			drawKey.fillRect(whiteKeyStartX[keyNum], (int)(height*0.3), blackKey-1, (int)(height*0.38));
+			drawKey.fillRect(whiteKeyStartX[keyNum] + 2*blackKey, (int)(height*0.3), blackKey, (int)(height*0.38));
+		}
+		//white key note assigning
+		if (keyNum % 7 == 0)
+			key = 12 * (keyNum / 7);
+		else if (keyNum % 7 == 1)
+			key = 2 + 12 * (keyNum / 7);
+		else if (keyNum % 7 == 2)
+			key = 4 + 12 * (keyNum / 7);
+		else if (keyNum % 7 == 3)
+			key = 5 + 12 * (keyNum / 7);
+		else if (keyNum % 7 == 4)
+			key = 7 + 12 * (keyNum / 7);
+		else if (keyNum % 7 == 5)
+			key = 9 + 12 * (keyNum / 7);
+		else if (keyNum % 7 == 6)
+			key = 11 + 12 * (keyNum / 7);
+		
+		drawKey.dispose();
+		channel.noteOn (key + 24, 100);     //play notes
+    }
+    
+    public void mousePressed (MouseEvent evt){
+	 
+    //If is in mouse mode
+    if(opt == true)
+    {
 	  int x = evt.getX();
-	  int y = evt.getY();
-	  
-	  int whiteKey = (width / 49) + 1;
-	  int blackKey = width / 147;
-	  
-	  boolean keyFound = false;
-	  
-	  Graphics g = getGraphics();
-	  g.setColor(Color.getHSBColor(0.61F, 0.67F, 1.0F));
+	  int y = evt.getY();	  
 	  
 	  //enter keyboard area
 	  if(y > height*0.30)
@@ -189,19 +326,8 @@ public class Piano extends JInternalFrame{
 			  {
 				  if (x >= blackKeyStartX[j] && x <= blackKeyStopX[j])
 				  {	
-					  g.fillRect(blackKeyStartX[j] + 1, (int)(height*0.3), 2*blackKey - 2, (int)(height*0.38));
 					  keyFound = true;
-					  //black key note assigning
-					  if (j % 5 == 0)
-						  key += 1 + 12 * (j / 5);
-					  else if (j % 5 == 1)
-						  key += 3 + 12 * (j / 5);
-					  else if (j % 5 == 2)
-						  key += 6 + 12 * (j / 5);
-					  else if (j % 5 == 3)
-						  key += 8 + 12 * (j / 5);
-					  else if (j % 5 == 4)
-						  key += 10 + 12 * (j / 5);
+					  pressBlack(j);
 					  break;
 				  }
 			  }
@@ -213,66 +339,94 @@ public class Piano extends JInternalFrame{
 			  {
 				  if (x >= whiteKeyStartX[i] && x <= whiteKeyStopX[i])
 				  {
-					g.fillRect(whiteKeyStartX[i], (int)(height*0.3), whiteKey - 1, (int)(height*0.7)); 
-					
-					
-	
-					g.setColor(Color.black);
-						
-					if((i + 1) % 7 == 1 || (i + 1) % 7 == 4)           //Draw black key on the right
-						g.fillRect(whiteKeyStartX[i] + 2*blackKey, (int)(height*0.3), 2*blackKey, (int)(height*0.38));
-					else if ((i + 1) % 7 == 0 || (i + 1) % 7 == 3)     //Draw black key on the left
-						g.fillRect(whiteKeyStartX[i], (int)(height*0.3), blackKey - 1, (int)(height*0.38));
-					else 	//draw black key on the centre
-					{
-						g.fillRect(whiteKeyStartX[i], (int)(height*0.3), blackKey, (int)(height*0.38));
-						g.fillRect(whiteKeyStartX[i] + 2*blackKey, (int)(height*0.3), blackKey, (int)(height*0.38));
-					}
-					//white key note assigning
-					if (i % 7 == 0)
-						key += 12 * (i / 7);
-					else if ( i % 7 == 1)
-						key += 2 + 12 * (i / 7);
-					else if (i % 7 == 2)
-						key += 4 + 12 * (i / 7);
-					else if (i % 7 == 3)
-						key += 5 + 12 * (i / 7);
-					else if (i % 7 == 4)
-						key += 7 + 12 * (i / 7);
-					else if (i % 7 == 5)
-						key += 9 + 12 * (i / 7);
-					else if (i % 7 == 6)
-						key += 11 + 12 * (i / 7);
+					pressWhite(i);
 					break;
 				}
-			}// end for
-		}// end keyFound == false
+			}
+		}
 	}// end keyboard area
-	
-	  channel.noteOn (key + 24, 100);     //play notes
-	  
+  }//end if
 }// end mousePressed
   
   public void mouseReleased(MouseEvent evt){
-	
+	  if (opt == true){
           channel.noteOff(key + 24, 100);
           repaint ();
-      
+	  }  
   }
-  
-  
   
   public void mouseEntered(MouseEvent evt) {}
   public void mouseExited(MouseEvent evt) {
 	      channel.noteOff(key + 24, 100);
   } 
   public void mouseClicked(MouseEvent evt){}
-  private int[] whiteKeyStartX = new int[49];
-  private int[] whiteKeyStopX = new int[49];
-  private int[] blackKeyStartX = new int[35];
-  private int[] blackKeyStopX = new int[35];
+  
+  
+  public void keyPressed(KeyEvent evt) {
+
+       int keyValue = evt.getKeyCode();	
+	
+       switch (keyValue){
+       case 'S':
+    	   pressWhite(octaveNum * 7);
+    	   break;
+       case 'D':
+    	   pressWhite((octaveNum * 7) + 1);
+    	   break;
+       case 'F':
+    	   pressWhite((octaveNum * 7) + 2);
+    	   break;
+       case 'J':
+    	   pressWhite((octaveNum * 7) + 3);
+    	   break;
+       case 'K':
+    	   pressWhite((octaveNum * 7) + 4);
+    	   break;
+       case 'L':
+    	   pressWhite((octaveNum * 7) + 5);
+    	   break;
+       case ';':
+    	   pressWhite((octaveNum * 7) + 6);
+    	   break;
+       case 'E':
+    	   pressBlack(octaveNum *5);
+    	   break;
+       case 'R':
+    	   pressBlack((octaveNum *5) + 1);
+    	   break;
+       case 'I':
+    	   pressBlack((octaveNum *5) + 2);
+    	   break;
+       case 'O':
+    	   pressBlack((octaveNum *5) + 3);
+    	   break;
+       case 'P':
+    	   pressBlack((octaveNum *5) + 4);
+    	   break;
+       case 'Z':
+    	   if(octaveNum > 0)
+    	   {
+    		   octaveNum -= 1;
+    		   repaint();
+    	   }
+    	   break;
+       case 'X':
+    	   if(octaveNum < 6)
+    	   {
+    		   octaveNum +=1;
+    		   repaint();
+    	   }
+    	   break;
+	}//end switch
+}
+
+public void keyReleased(KeyEvent evt) {
+	channel.noteOff(key + 24, 100);
+	repaint();
+}
+
+public void keyTyped(KeyEvent evt) { }
+
 }// end layout()
-  
-  
   
 }// end piano()
